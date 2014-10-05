408554fa3e90c4b3552e4f83f485cd84aa73d29c
Leon.R.Sine <leonsin112358@gmail.com>
2014-10-05 18:55:24 +0800

    implementation of rational class

--- progfun/.worksheet/src/week3.rationals.scala
+++ progfun/.worksheet/src/week3.rationals.scala
@@ -1,12 +1,12 @@
 package week3
 
 object rationals {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(62); 
-  val x = new Rational(1, 2);System.out.println("""x  : week3.Rational = """ + $show(x ));$skip(10); val res$0 = 
+  val x = new Rational(1, 3);System.out.println("""x  : week3.Rational = """ + $show(x ));$skip(29); 
+  val y = new Rational(5, 7);System.out.println("""y  : week3.Rational = """ + $show(y ));$skip(29); 
+  val z = new Rational(3, 2);System.out.println("""z  : week3.Rational = """ + $show(z ));$skip(10); val res$0 = 
   x.numer;System.out.println("""res0: Int = """ + $show(res$0));$skip(10); val res$1 = 
-  x.denom;System.out.println("""res1: Int = """ + $show(res$1));$skip(32); 
-  
-  val y = new Rational(2, 3);System.out.println("""y  : week3.Rational = """ + $show(y ));$skip(11); val res$2 = 
-  x.add(y);System.out.println("""res2: week3.Rational = """ + $show(res$2))}
+  x.denom;System.out.println("""res1: Int = """ + $show(res$1));$skip(18); val res$2 = 
+  x.sub(y).sub(z);System.out.println("""res2: week3.Rational = """ + $show(res$2))}
 }
 
 class Rational(x: Int, y: Int){
@@ -18,5 +18,9 @@ class Rational(x: Int, y: Int){
 			numer * that.denom + that.numer * denom,
 			denom * that.denom)
 			 
+	def neg: Rational = new Rational(-numer, denom)
+	
+	def sub(that: Rational) = add(that.neg)
+	
 	override def toString = numer + "/" + denom
 }
--- progfun/src/week3/rationals.sc
+++ progfun/src/week3/rationals.sc
@@ -1,12 +1,12 @@
 package week3
 
 object rationals {
-  val x = new Rational(1, 2)                      //> x  : week3.Rational = 1/2
+  val x = new Rational(1, 3)                      //> x  : week3.Rational = 1/3
+  val y = new Rational(5, 7)                      //> y  : week3.Rational = 5/7
+  val z = new Rational(3, 2)                      //> z  : week3.Rational = 3/2
   x.numer                                         //> res0: Int = 1
-  x.denom                                         //> res1: Int = 2
-  
-  val y = new Rational(2, 3)                      //> y  : week3.Rational = 2/3
-  x.add(y)                                        //> res2: week3.Rational = 7/6
+  x.denom                                         //> res1: Int = 3
+  x.sub(y).sub(z)                                 //> res2: week3.Rational = -79/42
 }
 
 class Rational(x: Int, y: Int){
@@ -18,5 +18,9 @@ class Rational(x: Int, y: Int){
 			numer * that.denom + that.numer * denom,
 			denom * that.denom)
 			 
+	def neg: Rational = new Rational(-numer, denom)
+	
+	def sub(that: Rational) = add(that.neg)
+	
 	override def toString = numer + "/" + denom
 }
