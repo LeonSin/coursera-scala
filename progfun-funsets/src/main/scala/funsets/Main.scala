10f9765270f883af82064570b5c3ff9f50603d88
Leon.R.Sine <leonsin112358@gmail.com>
2014-10-05 21:56:26 +0800

    first commit of exercise of week2

--- progfun-funsets/src/main/scala/funsets/Main.scala
+++ progfun-funsets/src/main/scala/funsets/Main.scala
@@ -2,5 +2,10 @@ package funsets
 
 object Main extends App {
   import FunSets._
+  printSet(singletonSet(1))
   println(contains(singletonSet(1), 1))
+  println(contains(singletonSet(1), 2))
+  printSet(union(singletonSet(1), singletonSet(2)))
+  printSet(intersect(union(singletonSet(1), singletonSet(2)), union(singletonSet(1), singletonSet(3))))
+  printSet(diff(union(singletonSet(1), singletonSet(2)), union(singletonSet(1), singletonSet(3))))
 }
